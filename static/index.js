a853b6527d16da0cd6910d962f9c1c06d95e43c8
QWQxiaoxiaoSama <98397373+QWQxiaoxiaoSama@users.noreply.github.com>
2022-01-26 14:40:43 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -348,11 +348,11 @@
             return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
         }
         SubmitResults();
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
-        return '人？';
+        if (score <= 49) return '嘶,你真是太逊了';
+        if (score <= 99) return '嘶，快但不完全快';
+        if (score <= 149) return '乂，不愧是你';
+        if (score <= 199) return '你真是太棒啦';
+        return 'W';
     }
 
     w.toStr = function(obj) {
